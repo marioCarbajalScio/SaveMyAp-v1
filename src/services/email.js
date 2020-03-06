1c552c8e4eef7978bf255f647f9516ef1defccb1
marioCarbajalScio <60710502+marioCarbajalScio@users.noreply.github.com>
2020-03-06 13:31:15 -0600

    Se arreglo correo

--- src/services/email.js
+++ src/services/email.js
@@ -15,32 +15,38 @@ function comas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function definir(x){
-  var definitions = ["","Type: ","Desing: ","Business Model: ","Login System: "
-  ,"Website Integration: ", "Users Profiles: ", "Admin Panel: ", "Languages: ","Progress: ","Estimated Dev Time: "
-]
-  var response = "";
-  var i = 1;
-    while(i<11){
-      response += [" [ "+definitions[i]+x[i] +" ] \n"]
-      i+=1
-    }
-    return response
-}
-
 //Funcion para mandar email
-export default async function sendEmail(e,total,definition) {
-
+export default async function sendEmail(e,total,ans) {
+  const quest = ["","Type: ","Desing: ","Business Model: ","Login System: "
+  ,"Website Integration: ", "Users Profiles: ", "Admin Panel: ", "Languages: ","Progress: ","Estimated Dev Time: "
+  ]
   var nextEmail = true;
-
-  console.log(definir(definition))
 
   const template_params = {
     "from_name": "Scio | End-to-end Software Engineering ",
     "name":e.get('user_name'),
     "email":e.get('user_email'), //Correo al que se mandara la info
     "message_html": "$"+comas(total), //La info de interes sobre el costo
-    "answers":definir(definition),
+    "question1":quest[1],
+    "answer1":ans[1],
+    "question2":quest[2],
+    "answer2":ans[2],
+    "question3":quest[3],
+    "answer3":ans[3],
+    "question4":quest[4],
+    "answer4":ans[4],
+    "question5":quest[5],
+    "answer5":ans[5],
+    "question6":quest[6],
+    "answer6":ans[6],
+    "question7":quest[7],
+    "answer7":ans[7],
+    "question8":quest[8],
+    "answer8":ans[8],
+    "question9":quest[9],
+    "answer9":ans[9],
+    "question10":quest[10],
+    "answer10":ans[10],
    }
 
    await emailjs.send(service_id, template_id, template_params, user_id )
@@ -57,7 +63,27 @@ export default async function sendEmail(e,total,definition) {
     "name":e.get('user_name'),
     "email":e.get('user_email'), 
     "email_scio": email_scio,
-    "message_html": "$"+comas(total)
+    "message_html": "$"+comas(total), //La info de interes sobre el costo
+    "question1":quest[1],
+    "answer1":ans[1],
+    "question2":quest[2],
+    "answer2":ans[2],
+    "question3":quest[3],
+    "answer3":ans[3],
+    "question4":quest[4],
+    "answer4":ans[4],
+    "question5":quest[5],
+    "answer5":ans[5],
+    "question6":quest[6],
+    "answer6":ans[6],
+    "question7":quest[7],
+    "answer7":ans[7],
+    "question8":quest[8],
+    "answer8":ans[8],
+    "question9":quest[9],
+    "answer9":ans[9],
+    "question10":quest[10],
+    "answer10":ans[10],
    }
 
    await emailjs.send(service_id, template_id_scio, template_params2, user_id )
